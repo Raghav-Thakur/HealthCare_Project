2c53f2d10d4fed7fb360771907f6665da9510b32
Raghav Thakur <you@example.com>
2024-10-22 15:52:02 +0530

    22-10-2024

---
+++ server/server.js
@@ -0,0 +1,32 @@
+// FRAMEWORK CONFIGURATION
+// --- Always Import/Require on top --- 
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+
+// Handles error so that the code doesnt breakdown and crash.
+// Middlewares -> acts as a guard
+// const errorHandler = require("./middleware/errorHandler");
+
+// Cross-Origin Resource Sharing (CORS) is a browser security feature that allows a web page to access resources from a different domain than the one that served the page
+const cors = require("cors");
+
+// env file config
+const dotenv = require("dotenv");
+dotenv.config();
+
+connectDb();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+// app.use(errorHandler);
+
+app.get('/',(req,res)=>{
+    res.send("working");
+});
+
+// APP CONFIG START
+app.listen(port, () => {
+    console.log("Server running on port https://localhost:${port}");
+});
